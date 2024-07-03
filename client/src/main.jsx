4a365ca4f60e3a1f98cabb04b855a205ea983128
Michaël Couffignal <michael@couffignal.fr>
2024-07-03 22:38:02 +0200

    creat header, adding app and header in router, change name of some path in router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,24 +4,33 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import HomePage from "./pages/HomePage";
 import FormPage from "./pages/FormPage";
 import QuizPage from "./pages/QuizPage";
 import "./styles/globals-utilities.css";
-
+HomePage
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <h1>404 Not Found</h1>,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "immersion",
+        element: <FormPage />,
+      },
+      {
+        path: "quiz",
+        element: <QuizPage />,
+      }
+    ],
   },
-  {
-    path:"formpage",
-    element: <FormPage />,
-  },
-  {
-    path: "quizpage",
-    element: <QuizPage />,
-  }
 ]);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
