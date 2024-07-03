ce68a2713ffd65cfe5393ab6cd60876f2c96fe84
julien44830 <deniaudjuli0@gmail.com>
2024-07-03 23:17:09 +0200

    reagencement du formulaire

--- client/src/components/TemplateForm.jsx
+++ client/src/components/TemplateForm.jsx
@@ -6,12 +6,15 @@ function TemplateForm (){
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe dolores, officiis officia illum consequuntur provident, facilis error quasi sint qui, voluptatum vitae? Illum, veniam et laboriosam earum quo unde libero.</p>
             </div>
             <form className="form">
+                <div className="div-form">
+
                 <input type="text" id="firstname" name="firstname" placeholder=" Prénom "/>
                 <input type="text" id="lastname" name="lastname" placeholder=" Nom "/>
                 <input type="email" id="email" name="email" placeholder=" Email "/>
                 <input type="password" id="password" name="password" placeholder=" Mot de passe"/>
                 <input type="password" id="password-confirmation" name="password-confirmation" placeholder=" Confirmation mot de passe"/>
                 <button type="submit">Valider</button>
+                </div>
             </form>
         </section>
     )
