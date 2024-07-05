761781be1ae7e548b0a5aa862dae47f37eab20a2
julien44830 <deniaudjuli0@gmail.com>
2024-07-05 05:52:38 +0200

    formulaire bien avance quiz bien avance nuit bien avance

--- client/src/components/TemplateForm.jsx
+++ client/src/components/TemplateForm.jsx
@@ -1,34 +1,116 @@
 import PropTypes from "prop-types";
+import { useRef, useEffect, useState } from "react";
 
-function TemplateForm({data} ) {
-console.log('%c⧭', 'color: #917399', data);
+function TemplateForm({ data, component }) {
+  const [tremble, setTremble] = useState(false);
+  console.log('%c⧭', 'color: #735656', tremble);
 
-    return (
-        <section className="template-form">
-            <aside className="text">
-                <h2 tabIndex="0">{data.h2}</h2>
-                <p tabIndex="0">{data.p}</p>
-            </aside>
-            <form className="form">
-                <input type="text" id="firstname" name="firstname" aria-label="Prénom" placeholder=" Prénom "/>
-                <input type="text" id="lastname" name="lastname" placeholder=" Nom "/>
-                <input type="email" id="email" name="email" placeholder=" Email "/>
-                <input type="password" id="password" name="password" placeholder=" Mot de passe"/>
-                <input type="password" id="password-confirmation" name="password-confirmation" placeholder=" Confirmation mot de passe"/>
-                <button type="submit" to="/quiz">Valider</button>
-            </form>
-        </section>
-    )
+  useEffect(() => {
+    if (component === 'Parkison') {
+      setTremble(true);
+    } else {
+      setTremble(false);
+    }
+  }, [component]);
+
+  console.log('%c⧭', 'color: #cc0088', component);
+  console.log('%c⧭', 'color: #917399', data);
+
+
+    const divRef = useRef(null);
+    const cursorRef = useRef(null);
+
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            const div = divRef.current;
+            const cursor = cursorRef.current;
+
+            if (div && cursor) {
+                const rect = div.getBoundingClientRect();
+                const mouseX = event.clientX - rect.left;
+                const mouseY = event.clientY - rect.top;
+
+                cursor.style.left = `${mouseX}px`;
+                cursor.style.top = `${mouseY}px`;
+                cursor.style.display = 'block';
+
+                // Ajoutez un effet de tremblement
+                const randomX = (Math.random() - 0.5) * 30; // Ajustez la valeur pour plus ou moins de tremblement
+                const randomY = (Math.random() - 0.5) * 30; // Ajustez la valeur pour plus ou moins de tremblement
+
+                cursor.style.transform = `translate(${randomX}px, ${randomY}px)`;
+            }
+        };
+
+        const handleMouseLeave = () => {
+            const cursor = cursorRef.current;
+            if (cursor) {
+                cursor.style.display = 'none';
+            }
+        };
+
+        const div = divRef.current;
+        if (div) {
+            div.addEventListener('mousemove', handleMouseMove);
+            div.addEventListener('mouseleave', handleMouseLeave);
+        }
+
+        return () => {
+            if (div) {
+                div.removeEventListener('mousemove', handleMouseMove);
+                div.removeEventListener('mouseleave', handleMouseLeave);
+            }
+        };
+    }, []);
+
+  return (
+    <section className="template-form">
+      <aside className="text">
+        <h2 tabIndex="0">{data.h2}</h2>
+        <p tabIndex="0">{data.p}</p>
+      </aside>
+      {tremble ? (
+        <div className="shaking-cursor-div" ref={divRef}>
+          <div className="custom-cursor" ref={cursorRef} />
+          <form className="form">
+            <label htmlFor="firstname">Prénom</label>
+            <input type="text" id="firstname" name="firstname" aria-label="Prénom" placeholder=" Prénom " />
+            <label htmlFor="lastname">Nom</label>
+            <input type="text" id="lastname" name="lastname" placeholder=" Nom " />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder=" Email " />
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" id="password" name="password" placeholder=" Mot de passe" />
+            <label htmlFor="password-confirmation">Confirmation mot de passe</label>
+            <input type="password" id="password-confirmation" name="password-confirmation" placeholder=" Confirmation mot de passe" />
+            <button type="submit">Valider</button>
+          </form>
+        </div>
+      ) : (
+        <form className="form">
+          <label htmlFor="firstname">Prénom</label>
+          <input type="text" id="firstname" name="firstname" aria-label="Prénom" placeholder=" Prénom " />
+          <label htmlFor="lastname">Nom</label>
+          <input type="text" id="lastname" name="lastname" placeholder=" Nom " />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder=" Email " />
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" id="password" name="password" placeholder=" Mot de passe" />
+          <label htmlFor="password-confirmation">Confirmation mot de passe</label>
+          <input type="password" id="password-confirmation" name="password-confirmation" placeholder=" Confirmation mot de passe" />
+          <button type="submit">Valider</button>
+        </form>
+      )}
+    </section>
+  );
 }
 
 TemplateForm.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      h2: PropTypes.string.isRequired,
-      p: PropTypes.string.isRequired
-    })
-  ).isRequired
+  data: PropTypes.shape({
+    h2: PropTypes.string.isRequired,
+    p: PropTypes.string.isRequired
+  }).isRequired,
+  component: PropTypes.string.isRequired
 };
 
 export default TemplateForm;
-
--- client/src/pages/FormPage.jsx
+++ client/src/pages/FormPage.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import TemplateForm from "../components/TemplateForm";
 import NavForm from "../components/NavForm";
 import BlindForm from "../components/BlindForm";
-import Parkison from "../components/Parkinson";
 import Daltonien from "../components/Daltonien";
 import Trouble from "../components/Trouble";
 
@@ -18,34 +17,33 @@ const data = {
     component : Daltonien,
     name: "Daltonien",
     h2: "daltonisme",
-    p: "daltonisme Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod",
+    p: "Le daltonisme est une altération génétique de la vision des couleurs, principalement affectant la distinction entre le rouge et le vert. Cela est dû à des anomalies des cônes rétiniens responsables de la perception des couleurs. Bien que généralement sans impact sur la santé physique, cette condition peut poser des défis dans des contextes nécessitant une perception précise des couleurs, comme dans les métiers artistiques ou la signalisation.",
   },
   Trouble : {
-    component : Trouble,
+    component : "",
     name: "Trouble",
     h2: "trouble de l'attention",
-    p: "trouble de l'attention Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod",
+    p: "Le trouble de l'attention, ou TDA (Trouble Déficitaire de l'Attention), est une condition neurologique caractérisée par des difficultés à maintenir l'attention, à contrôler les impulsions et à réguler son comportement. Les personnes atteintes peuvent être facilement distraites, avoir du mal à terminer les tâches et être impulsives. Ce trouble peut interférer significativement avec la vie quotidienne, notamment à l'école et au travail, nécessitant souvent une gestion adaptative et un soutien spécialisé.",
   },
   Parkison : {
-    component : Parkison,
+    component : "",
     name: "parkison",
     h2: "parkison",
-    p: "parkison Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod",
+    p: "La maladie de Parkinson est une affection neurodégénérative chronique qui affecte le système nerveux central. Elle se caractérise par la diminution progressive de la production de dopamine, un neurotransmetteur crucial pour le contrôle des mouvements et d'autres fonctions cognitives. Les symptômes incluent tremblements, raideur musculaire, lenteur des mouvements et problèmes d'équilibre. Bien qu'il n'existe pas de remède, divers traitements peuvent atténuer les symptômes et améliorer la qualité de vie des patients.",
   },
 };
 
 function FormPage() {
     const [handicap, setHandicap] = useState(0);
-    const [component, setComponent] = useState("Trouble")
-    console.log("ici c'est le component", component)
+    const [component, setComponent] = useState("Parkison")
     const ComponentToRender = data[component].component;
 
   return (
     <main className="formpage">
       <h1 tabIndex="0">Découvrez la réalité du handicap sur internet</h1>
       <NavForm setHandicap={setHandicap} setComponent={setComponent} handicap={handicap} />
-      {component !== "BlindForm" ? 
-      <TemplateForm data={data[component]} /> : null }
+      {component !== "BlindForm"  ? 
+      <TemplateForm data={data[component]} component={component} /> : null }
       {ComponentToRender && <ComponentToRender />} 
       
     </main>
