276cf404cbd3a1dffd3624f02ad604e3fd29e5a3
Michaël Couffignal <michael@couffignal.fr>
2024-07-09 19:36:43 +0200

    on enregistre, fin de journner

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -6,6 +6,36 @@ function Question({ data, setNumb, numb }) {
   console.log('%c⧭', 'color: #997326', "reponse", reponse);
   console.log('%c⧭', 'color: #997326', "reponse", data.reponse);
 
+  // const [true, setTrue] = useState("");
+  // const [false, setFalse] = useState("");
+
+  // on va tester ça :
+
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [isLocked, setIsLocked] = useState(false);
+
+  const handleOptionChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedOption(selectedValue);
+    setIsLocked(true);
+  };
+
+  // <label className={selectedOption === 'option1' ? 'selected' : ''}>
+  // <input
+  //   type="radio"
+  //   name="quiz"
+  //   value="option1"
+  //   checked={selectedOption === 'option1'}
+  //   onChange={handleOptionChange}
+  //   disabled={isLocked && selectedOption !== 'option1'}
+  // />
+  // <span>Option 1</span>
+
+
+  // fin du test, et bientot fin de chantier :)
+
+
+
   const selectReponse = (e) => {
     setReponse(e.target.value);
   };
@@ -13,22 +43,26 @@ function Question({ data, setNumb, numb }) {
   useEffect(() => {
     if (data.reponse === reponse) {
       console.log("gagné");
+      setTrue("question-true")
+      // input + span (qui sont dans un label, qui est selectionné)
+      //  = green (le background? juste le border?)
     }
 
-    if(numb===19){
-      setNumb( 0)
+    if (numb === 19) {
+      setNumb(0)
+      // afficher un component resultat avec nombre de bonne rep
     }
   }, [reponse, data.reponse], numb);
-  
-  const cuntNum= ()=>{
-    setNumb(numb +1)
+
+  const cuntNum = () => {
+    setNumb(numb + 1)
   }
 
   return (
     <section className="question">
       <h1>{data.Question}</h1>
 
-      
+
       <fieldset role="radiogroup" className="section-radio-vertical">
         <label className="label-radio" htmlFor={`prop1-${data.id}`}>
           <input
